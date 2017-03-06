fef7e201f0077808c2eb6c4e6eb3a35e9480d5e9
keith <keith@iamkeithcarter.com>
2017-03-05 23:39:50 -0500

    fixed issue with extra , in the js file

--- js/kc.js
+++ js/kc.js
@@ -8,11 +8,11 @@
             
             var $theRepos = $('#theRepos')
    
-//            $("button").click(function(){
+
                 $.get("https://api.github.com/users/iamkeithcarter", function(data, status){
 
                     var  item= data.repos_url;
-//                     $theRepos.append('<li>'+item+'</li>');
+
                         
                     $.get(item,function(data,status){
 
@@ -21,15 +21,15 @@
                             var link = repo.html_url;
                             var name = repo.name;
                             var updatedDate = repo.updated_at;
-//                          $theRepos.append('<li>'+repo+'</li>'); this is the object
-                            $theRepos.append('<li><a href="'+link+'">'+name+'</a></li>');
-//                            $theRepos.append('<li>'+name+'</li>');
-                            $theRepos.append('<li>'+updatedDate+'</li>');
+
+                            $theRepos.append('<li><a href="'+link+'">'+name+'</a li>');
+
+                          
                         
                         //These pull the exact element by name out of the repo.
                         
                         }
                     });      
         });
-//    }); the button click script
+
 });
